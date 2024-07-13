feda71d0a4eb7b28215eb6fb3a2e75ce8475c98c
Thiago <lucimara1422@gmail.com>
2024-07-13 16:33:53 -0300

    feat(kill) vite and next app

--- apps/vite/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@hodler/eslint-config/vite.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
--- apps/vite/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import config from '@hodler/ui/config';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [config],
-  content: ['./src/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}', '../../packages/ui/tailwind.config.js'],
-};
