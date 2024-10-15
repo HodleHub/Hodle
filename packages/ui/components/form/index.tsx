13ab7c350d8f432bcadda91f2dc74381decfc610
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-10-15 20:19:54 -0300

    feat(ui) add ui

---
+++ packages/ui/components/form/index.tsx
@@ -0,0 +1,11 @@
+import { Label } from './Label'
+import { InputField } from './InputField'
+import { Field } from './Field'
+import { ErrorMessage } from './ErrorMessage'
+
+export const ContentForm = {
+  Label,
+  Field,
+  InputField,
+  ErrorMessage,
+}
