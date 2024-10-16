ecfd3efb1e42afcb80c78d6e56f51c72fb5bad89
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-10-16 08:31:05 -0300

    feat(front) try a new front end

--- apps/dash/src/App.tsx
+++ apps/dash/src/App.tsx
@@ -2,7 +2,8 @@ import Routes from './routes/Routes';
 
 const App = () => {
   return (
-    <Routes />
+    // <Routes />
+    <div className='text-red-300'>Hello world</div>
   );
 };
 
--- apps/dash/tailwind.config.js
+++ apps/dash/tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
-import uiConfig from '@hodler/ui/config';
+// import uiConfig from '@hodler/ui/config';
 
 module.exports = {
   ...uiConfig,
   content: [
-    ...uiConfig.content, 
+    // ...uiConfig.content, 
     './src/**/*.{js,jsx,ts,tsx}', 
   ],
   theme: {
-    ...uiConfig.theme,
+    // ...uiConfig.theme,
     extend: {
-      ...uiConfig.theme.extend,
+      // ...uiConfig.theme.extend,
       colors: {
         customColor: "#ff5733",
-        ...uiConfig.theme.extend.colors,
+        // ...uiConfig.theme.extend.colors,
       },
     },
   },
   plugins: [
-    ...uiConfig.plugins, 
+    // ...uiConfig.plugins, 
   ],
 };
